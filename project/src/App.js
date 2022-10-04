3f88d3609012fa2607e1d8909b04a420ef6029d8
Cesar Solano <171cas@gmail.com>
2022-10-03 22:52:47 -0500

    basic routes done

--- project/src/App.js
+++ project/src/App.js
@@ -1,3 +1,5 @@
+import { Switch, Route, NavLink } from 'react-router-dom'
+
 import './App.css';
 
 function App() {
@@ -5,6 +7,19 @@ function App() {
     <>
       <h1> {'>>>'} Incredible Superhero Store {'<<<'} </h1>
       <h1>Welcome Comic Lover!</h1>
+      <div>
+        <NavLink to="/">Home</NavLink>
+        <br />
+        <NavLink to="/newcharacter">New Character</NavLink>
+        <br />
+        <NavLink to='/character/1'>TEST::REMOVE Single Character</NavLink>
+        <br />
+      </div>
+      <Switch>
+        <Route path='/' exact>Home Page</Route>
+        <Route path='/newcharacter'>New Character Form</Route>
+        <Route path='/character/:charId'>Single Character Detail</Route>
+      </Switch>
     </>
   );
 }
