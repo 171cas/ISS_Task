789758ef8cf2d1bd7a6c01846829509c14925ff7
Cesar Solano <171cas@gmail.com>
2022-10-03 22:29:10 -0500

    base done

---
+++ project/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1> {'>>>'} Incredible Superhero Store {'<<<'} </h1>
+      <h1>Welcome Comic Lover!</h1>
+    </>
+  );
+}
+
+export default App;
